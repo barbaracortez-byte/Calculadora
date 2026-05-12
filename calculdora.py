80cb6409ac6833284bb307784f213f958d3b7f87
barbara <barbara.cortez@edu.senai.br>
2026-05-12 15:17:56 -0300

    Arquivo alterado , cabeçalho removido

--- calculdora.py
+++ calculdora.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Meu Primeiro Programa em Python
-Autor: [Seu Nome]
-Data: [Data de Hoje]
-Descrição: Programa básico para aprender GitHub
-"""
-
+# -*- coding: utf-8 -*-s
+s
 # Importações
 import datetime
 import random
