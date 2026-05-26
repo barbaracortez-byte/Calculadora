54221719ccb182f4252f3465bcd64e4a7a846316
barbara <barbara.cortez@edu.senai.br>
2026-05-26 10:55:27 -0300

    Adiciona função de despedida

--- saudacao.py
+++ saudacao.py
@@ -1,8 +1,3 @@
-def saudacao(nome):
-  return f'Tenha um otimo final de semana, {nome} '
+def despedida (nome):
+    return f"Até mais, {nome}!"
 
-def soma(n1,n2):
-  resultado = n1 + n2
-  return(resultado)
-
-print(saudacao('Barbara'))
